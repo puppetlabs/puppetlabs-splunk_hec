cbc80a65072d7397d326301130c3f6c3c59770af
Chris Barker <cbarker@puppet.com>
2018-10-16 20:57:37 +1100

    default to pcp

--- plans/deploy.pp
+++ plans/deploy.pp
@@ -3,11 +3,14 @@ plan splunk_hec::deploy {
   # query to get CA 'resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Certificate_authority"}'
   $result_ca = puppetdb_query('resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Certificate_authority"}')
   $ca = $result_ca.map |$r| { $r["certname"] }
+  $pcpca = $ca.map |$n| { "pcp://${n}" }
+
   # query to get other masters 'resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Master"}'
   $result_master = puppetdb_query('resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Master"}')
   $masters = $result_master.map |$r| { $r["certname"] }
+  $pcpmasters = $masters.map |$n| { "pcp://${n}" }
 
-  run_command('/opt/puppetlabs/bin/puppet agent -t', $ca)
-  run_command('/opt/puppetlabs/bin/puppet agent -t', $masters)
+  run_command('/opt/puppetlabs/bin/puppet agent -t', $pcpca)
+  run_command('/opt/puppetlabs/bin/puppet agent -t', $pcpmasters)
 
 }
