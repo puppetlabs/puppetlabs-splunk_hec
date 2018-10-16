38c114681189137686d1117f214680a60c52266c
Chris Barker <cbarker@puppet.com>
2018-10-16 21:17:39 +1100

    Remove duplicate run on CAs

--- plans/deploy.pp
+++ plans/deploy.pp
@@ -1,16 +1,17 @@
 plan splunk_hec::deploy {
 
-  # query to get CA 'resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Certificate_authority"}'
+  # Lets find the CA's first
   $result_ca = puppetdb_query('resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Certificate_authority"}')
   $ca = $result_ca.map |$r| { $r["certname"] }
   $pcpca = $ca.map |$n| { "pcp://${n}" }
 
-  # query to get other masters 'resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Master"}'
-  $result_master = puppetdb_query('resources[certname] { type = "Class" and title = "Puppet_enterprise::Profile::Master"}')
+  # Now we find the compile masters
+  $result_master = puppetdb_query('resources[certname] { (type = "Class" and title = "Puppet_enterprise::Profile::Master") and !(type = "Class" and title = "Puppet_enterprise::Profile::Certificate_authority")}')
   $masters = $result_master.map |$r| { $r["certname"] }
   $pcpmasters = $masters.map |$n| { "pcp://${n}" }
 
-  run_command('/opt/puppetlabs/bin/puppet agent -t', $pcpca)
-  run_command('/opt/puppetlabs/bin/puppet agent -t', $pcpmasters)
+  run_command('/opt/puppetlabs/bin/puppet agent --onetime --no-usecacheonfailure --no-daemonize --no-splay', $pcpca, '_concurrency' => '1')
+  
+  run_command('/opt/puppetlabs/bin/puppet agent --onetime --no-usecacheonfailure --no-daemonize --no-splay', $pcpmasters, '_concurrency' => '1')
 
 }
