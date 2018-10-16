c676b115c1bbed0920536f343c79010270b10a6f
Chris Barker <cbarker@puppet.com>
2018-10-16 17:39:01 +1100

    add Class

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class splunk_hec (
   String $server,
   Sensitive $token,
   Optional[String] $puppetdb_callback_hostname,
-  Optional[Integar] $port,
-  Optional[Integar] $timeout,
+  Optional[Integer] $port,
+  Optional[Integer] $timeout,
 ) {
 
   file { '/etc/puppetlabs/puppet/splunk_hec.yaml':
