ed363c5e042b550b2086528ae28657c06b43f175
Chris Barker <cbarker@puppet.com>
2019-06-13 09:58:32 +0100

    Includes updated manifest in commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,10 @@ class splunk_hec (
   Array $collect_facts = ['dmi','disks','partitions','processors','networking'],
   Boolean $enable_reports = false,
   Boolean $record_event = false,
+  Boolean $manage_routes = false,
   String $reports = 'puppetdb,splunk_hec',
+  String $facts_terminus = 'puppetdb',
+  String $facts_cache_terminus = 'splunk_hec',
   Optional[String] $pe_console = undef,
   Optional[Integer] $timeout = undef,
   Optional[String] $ssl_ca = undef,
@@ -26,6 +29,26 @@ class splunk_hec (
       section => 'master',
       setting => 'reports',
       value   => $reports,
+      notify  => Service['pe-puppetserver'],
+    }
+  }
+
+  if $manage_routes {
+    file { '/etc/puppetlabs/puppet/splunk_hec_routes.yaml':
+      ensure  => file,
+      owner   => pe-puppet,
+      group   => pe-puppet,
+      mode    => '0640',
+      content => epp('splunk_hec/splunk_hec_routes.yaml.epp'),
+      notify  => Service['pe-puppetserver'],
+    }
+    pe_ini_setting {'enable splunk_hec_routes.yaml':
+      ensure  => present,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'master',
+      setting => 'route_file',
+      value   => '/etc/puppetlabs/puppet/splunk_hec_routes.yaml',
+      require => File['/etc/puppetlabs/puppet/splunk_hec_routes.yaml'],
       notify  => Service['pe-puppetserver'],
     }
   }
