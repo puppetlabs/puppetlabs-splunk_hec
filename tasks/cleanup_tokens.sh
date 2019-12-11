a9b6c2648aa67ee0fdd87318776552067be7d570
Chris Barker <cbarker@puppet.com>
2019-12-11 14:52:59 -0800

    Removes the possibility of user input
    Since the Splunk App labeled the tokens, this only revokes the tokens created by that.
    
    No longer needed using input, this becomes a much safer script to run (and wont kick a user out of a console session if they used their own account to generate the token)

--- tasks/cleanup_tokens.sh
+++ tasks/cleanup_tokens.sh
@@ -6,10 +6,11 @@ declare -x CURL='/bin/curl'
 SSLDIR=$($PUPPET config print ssldir --section master)
 CERTNAME=$($PUPPET config print certname --section master)
 
-USERNAME=$PT_username
+# we want to only delete the hard coded token called 'splunk report viewer' so will not take user input
 
-$CURL -X DELETE "https://$CERTNAME:4433/rbac-api/v2/tokens?revoke_tokens_by_usernames=$USERNAME" \
+$CURL -X DELETE "https://$CERTNAME:4433/rbac-api/v2/tokens" \
   --tlsv1 \
   --cacert $SSLDIR/certs/ca.pem \
   --cert $SSLDIR/certs/$CERTNAME.pem \
-  --key $SSLDIR/private_keys/$CERTNAME.pem
+  --key $SSLDIR/private_keys/$CERTNAME.pem \
+  -d '{"revoke_tokens_by_labels": ["splunk report viewer"]}'
