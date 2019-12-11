dda5e39b7e6333e094bf0fac7c201456220ce61a
Chris Barker <cbarker@puppet.com>
2019-12-11 15:57:48 -0800

    Reverts to username
    Tokens can only be revoked by label by user who created them, since we are using the api_user in this instance, we should just use the username
    
    String is handled better and embedded in json post instead of just inline command

--- tasks/cleanup_tokens.sh
+++ tasks/cleanup_tokens.sh
@@ -6,11 +6,11 @@ declare -x CURL='/bin/curl'
 SSLDIR=$($PUPPET config print ssldir --section master)
 CERTNAME=$($PUPPET config print certname --section master)
 
-# we want to only delete the hard coded token called 'splunk report viewer' so will not take user input
+USERNAME="$PT_username"
 
 $CURL -X DELETE "https://$CERTNAME:4433/rbac-api/v2/tokens" \
   --tlsv1 \
   --cacert $SSLDIR/certs/ca.pem \
   --cert $SSLDIR/certs/$CERTNAME.pem \
   --key $SSLDIR/private_keys/$CERTNAME.pem \
-  -d '{"revoke_tokens_by_labels": ["splunk report viewer"]}'
+  -d "{\"revoke_tokens_by_usernames\": [\"$USERNAME\"]}"
