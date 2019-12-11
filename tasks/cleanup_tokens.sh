3c42076f3b0a4ef4a63fc4373c2851ff639b1f19
Chris Barker <cbarker@puppet.com>
2019-12-10 18:27:23 -0800

    Adds a task that allows a user to revoke Tokens
    This is a fix for behavior introduced by the report viewer on the splunk side that could leave stale tokens around for a long time.

---
+++ tasks/cleanup_tokens.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -x PUPPET='/opt/puppetlabs/bin/puppet'
+declare -x CURL='/bin/curl'
+
+SSLDIR=$($PUPPET config print ssldir --section master)
+CERTNAME=$($PUPPET config print certname --section master)
+
+USERNAME=$PT_username
+
+$CURL -X DELETE "https://$CERTNAME:4433/rbac-api/v2/tokens?revoke_tokens_by_usernames=$USERNAME" \
+  --tlsv1 \
+  --cacert $SSLDIR/certs/ca.pem \
+  --cert $SSLDIR/certs/$CERTNAME.pem \
+  --key $SSLDIR/private_keys/$CERTNAME.pem
