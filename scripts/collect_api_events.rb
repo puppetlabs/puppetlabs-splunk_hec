a9b99492bdd3b9588a49d3384ba16b28ce3e0334
Greg Hardy <greg.hardy@puppetlabs.com>
2020-11-27 17:10:06 +0000

    (PIE-421) Added cron resource to collect API events

--- scripts/collect_api_events.rb
+++ scripts/collect_api_events.rb
@@ -1,3 +1,5 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
 require 'common_events_library'
 require 'benchmark'
 require 'json'
@@ -11,7 +13,7 @@ def load_settings(configfile)
 end
 
 def transform(body, pe_console, source_type, splunk_client, splunk_token)
-  events_json = ''
+  events = ''
   body.each do |pe_event|
     timestamp = Time.new
     event = {
@@ -20,11 +22,10 @@ def transform(body, pe_console, source_type, splunk_client, splunk_token)
       'sourcetype' => source_type,
       'event' => pe_event,
     }
-    response = splunk_client.post_request('/services/collector', event, {'Authorization': "Splunk #{splunk_token}"})
-    puts response
-    raise "Failed to POST to the splunk server [#{response.error!}]" unless response.code == '200'
+    events = events + ' ' unless events.empty?
+    events = events + event.to_json
   end
-  events_json
+  events
 end
 
 def parse_body(response_body)
@@ -37,8 +38,7 @@ def parse_body(response_body)
   body
 end
 
-config_file = ENV['CONFIG_FILE'] || 'conf/splunk_config.yaml'
-
+config_file = ENV['CONFIG_FILE'] || "#{File.expand_path(File.dirname(__FILE__))}/../conf/splunk_config.yaml"
 settings = load_settings(config_file)
 
 splunk_client = CommonEventsHttp.new('http://' + settings['splunk']['server'], port: settings['splunk']['port'], ssl_verify: false)
@@ -47,14 +47,29 @@ response = orchestrator.get_all_jobs
 raise "Failed to get the jobs from PE [#{response.error!}]" unless response.code == '200'
 
 body = parse_body(response.body)
-transform(body['items'], settings['pe']['console'], 'puppet:summary', splunk_client, settings['splunk']['token'])
 
-puts "Post of orchestrator events successful"
+events_json = transform(body['items'], settings['pe']['console'], 'puppet:summary', splunk_client, settings['splunk']['token'])
+
+unless events_json.empty?
+  response = splunk_client.post_request('/services/collector', events_json, {'Authorization': "Splunk #{settings['splunk']['token']}"}, use_raw_body: true)
+  raise "Failed to POST to the splunk server [#{response.error!}]" unless response.code == '200'
+
+  puts "Post of orchestrator events successful"
+else
+  puts "There were no orchestrator events"
+end
 
 events = Events.new(settings['pe']['console'], settings['pe']['username'], settings['pe']['password'], ssl_verify: false)
 response = events.get_all_events
 raise "Failed to get the activity API events from PE [#{response.error!}]" unless response.code == '200'
 
 body = parse_body(response.body)
-transform(body['commits'], settings['pe']['console'], 'puppet:summary', splunk_client, settings['splunk']['token'])
-puts "Post of activity service events successful"
+activity_json = transform(body['commits'], settings['pe']['console'], 'puppet:summary', splunk_client, settings['splunk']['token'])
+unless activity_json.empty?
+  response = splunk_client.post_request('/services/collector', activity_json, {'Authorization': "Splunk #{settings['splunk']['token']}"}, use_raw_body: true)
+  raise "Failed to POST to the splunk server [#{response.error!}]" unless response.code == '200'
+
+  puts "Post of activity service events successful"
+else
+  puts "There were no activity service events"
+end
