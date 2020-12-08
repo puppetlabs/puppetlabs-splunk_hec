1176b0373b0b8d6235d15012ee1d9e32c7461caf
Greg Hardy <greg.hardy@puppetlabs.com>
2020-12-08 11:47:00 +0000

    (PIE-445) Add the resource cron job to the init.pp
    Document the settings
    Document the install instructions
    Add a optional param to turn off the collection

--- manifests/collect_events.pp
+++
@@ -1,6 +0,0 @@
-cron { 'collectpeapi':
-  ensure  => 'present',
-  command => '/etc/puppetlabs/code/environments/production/modules/splunk_hec/scripts/collect_api_events.rb',
-  user    => 'root',
-  minute  => '*/2',
-}
