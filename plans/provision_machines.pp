4fae73f591a42ef82c5049655543f4e0dae0c19d
nam054 <nam054@users.noreply.github.com>
2021-06-29 16:47:19 +0000

    Release prep to 0.9.0
    You will need to manually update the `CHANGELOG.md` file. First, checkout this PR on your local machine via something like `git fetch upstream; git checkout upstream/release_prep`. Then, once you have updated the `CHANGELOG.md` file, `git commit --amend` your update, then push your changes to this PR via something like `git push --set-upstream upstream.`
    
     **Note:** You can use https://github.com/puppetlabs/puppetlabs-splunk_hec/compare/v0.8.1...main to see all the new commits that have landed since the previous release.

--- plans/provision_machines.pp
+++ plans/provision_machines.pp
@@ -1,3 +1,9 @@
+# @summary Provisions machines
+#
+# @param [Optional[String]] using
+#   provision service
+# @param [Optional[String]] image
+#   os image
 plan splunk_hec::provision_machines(
   Optional[String] $using = 'abs',
   Optional[String] $image = 'centos-7-x86_64'
