fa56a307c11eb927274086ea6e103fd324c27e90
Bill Hurt <hurt.bill@gmail.com>
2021-09-15 08:13:56 -0700

    (PIE-859) Add PE to test matrix
    This change adds PE versions to the integration test matrix which was
    previously Open Source only.
    
    This is required because we now need to test PE Event forwarding as
    well.

---
+++ plans/acceptance/pe_server_setup.pp
@@ -0,0 +1,34 @@
+# @summary Install PE Server
+#
+# Install PE Server
+#
+# @example
+#   pe_event_forwarding::acceptance::pe_server_setup
+plan splunk_hec::acceptance::pe_server_setup(
+  Optional[String] $version = '2019.8.7',
+  Optional[Hash] $pe_settings = {password => 'puppetlabs'}
+) {
+  # machines are not yet ready at time of installing the puppetserver, so we wait 15s
+  $localhost = get_targets('localhost')
+  run_command('sleep 15s', $localhost)
+
+  #identify pe server node
+  $puppet_server =  get_targets('*').filter |$n| { $n.vars['role'] == 'server' }
+
+  # install pe server
+  run_plan(
+    'deploy_pe::provision_master',
+    $puppet_server,
+    'version' => $version,
+    'pe_settings' => $pe_settings
+  )
+
+  $cmd = @("CMD")
+          puppet infra console_password --password=pie
+          echo 'pie' | puppet access login --lifetime 1y --username admin
+          puppet infrastructure tune | sed "s,\\x1B\\[[0-9;]*[a-zA-Z],,g" > /etc/puppetlabs/code/environments/production/data/common.yaml
+          puppet agent -t
+          | CMD
+
+  run_command($cmd, $puppet_server, '_catch_errors' => true)
+}
---
+++ plans/acceptance/server_setup.pp
@@ -0,0 +1,25 @@
+# @summary Install PE Server
+#
+# Install PE Server
+#
+# @example
+#   pe_event_forwarding::acceptance::pe_server
+plan splunk_hec::acceptance::server_setup(
+  Optional[String] $puppet_version = '2019.8.7',
+) {
+  # machines are not yet ready at time of installing the puppetserver, so we wait 15s
+  $localhost = get_targets('localhost')
+  run_command('sleep 15s', $localhost)
+
+  if $puppet_version =~ /puppet/ {
+    run_plan(
+      'splunk_hec::acceptance::oss_server_setup',
+      'collection' => $puppet_version
+    )
+  } else {
+    run_plan(
+      'splunk_hec::acceptance::pe_server_setup',
+      'version' => $puppet_version
+    )
+  }
+}
